79d94a7ef481265c9a89d91b98e6e3bd6876aece
Chie <akiba.chie@gmail.com>
2017-01-20 13:07:39 -0800

    Seeing if this works

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 //For server
-require('babel-register');
+require('babel-core/register')({ presets: ['es2015', 'react'] });
 require('babel-polyfill');
+require('import-export');
 require('./server/app');
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const react = require('react');
+const reactDOMServer = require('react-dom/server');
+
+const renderToString = reactDOMServer.renderToString;
