2273fb4d6c534efa984739368c3d77e0d9091b6b
Chie <akiba.chie@gmail.com>
2017-01-20 16:12:10 -0800

    See if this works

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.keys = {
   'APIKEY': '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',
   'email': 'akiba.chie@gmail.com',
-  'MONGODB_URI': 'mongodb://<chieakiba>:<etoile921>@ds117109.mlab.com:17109/oinkster'
+  'MONGODB_URI': 'mongodb://chieakiba:etoile921@ds117109.mlab.com:17109/oinkster'
 }
